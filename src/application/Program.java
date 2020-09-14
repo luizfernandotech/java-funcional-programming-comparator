13a5a24804f49f2b938c3058d6315f934b26e57c
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2020-09-14 21:06:31 +0100

    Removed MyComparator class. Created a comparator anonymous class

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import entities.Product;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Program {
@@ -16,7 +17,15 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 400.00));
 
-        list.sort(new MyComparator());
+        // anonymous class comparator
+        Comparator<Product> comparator = new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+            }
+        };
+
+        list.sort(comparator);
 
         for (Product p : list) {
             System.out.println(p);
